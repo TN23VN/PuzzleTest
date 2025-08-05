3320b4061168d86ca9172e907eb3d5a4d5f32e7d
TN23VN <tronghv9abg@gmail.com>
2025-08-05 19:38:13 +0700

    commit
    update gameplay logic

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Game : MonoBehaviour
 {
@@ -40,11 +41,60 @@ public class Game : MonoBehaviour
         List<Cube> bottle1Cubes = bottle1.cubes;
         List<Cube> bottle2Cubes = bottle2.cubes;
 
-        Cube c = bottle1Cubes[0];
-        bottle1Cubes.RemoveAt(0);
+        if(bottle1Cubes.Count == 0) { return; }
+        if(bottle2Cubes.Count == 4) { return; }
 
-        bottle2Cubes.Add(c);
-    }    
+        int index = bottle1Cubes.Count - 1;
+        Cube c = bottle1Cubes[index];
+
+        var type = c.type;
+        if (bottle2Cubes.Count > 0 && bottle2Cubes[bottle2Cubes.Count-1].type != type)
+        {
+            return;
+        }
+
+        for (int i = index; i >= 0; i--) 
+        {
+            Cube cube = bottle1Cubes[i];
+            if (cube.type == type)
+            {
+                bottle1Cubes.RemoveAt(i);
+                bottle2Cubes.Add(cube);
+            }
+            else {break;}
+        }
+        
+    }
+
+    public bool CheckWinCondition()
+    {
+        bool winFlag = true;
+
+        foreach (Bottle bottle in bottles)
+        {
+            if (bottle.cubes.Count == 0)
+                continue;
+
+            if (bottle.cubes.Count < 4)
+            {
+                winFlag = false; break;
+            }
+            bool sameTypeFlag = true;
+            CubeType type = bottle.cubes[0].type;
+            foreach (Cube cube in bottle.cubes)
+            {
+                if (cube.type != type)
+                {
+                    sameTypeFlag = false; break;
+                }
+            }
+            if (!sameTypeFlag)
+            {
+                winFlag = false; break;
+            }
+        }
+        return winFlag;
+    }
 
     public class Bottle
     {
